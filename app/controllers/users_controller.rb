bbf31d1bef336584d386e24d76a58d467ad8b05c
onokusuna <piroshki4315@gmail.com>
2020-04-17 19:50:50 +0900

    会員edit画面作成 保存できない不具合発生

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,22 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+  	if current_user.update(user_params)
+  	   redirect_user_path(current_user)
+  	else
+  	   redirect_root_path
+  	end
+  end
+
   def leave
   end
 
   def thanks
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
