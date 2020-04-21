507d460eecea4e4b9717926b09a2dc21dc1157ac
onokusuna <piroshki4315@gmail.com>
2020-04-21 15:43:42 +0900

    展覧会サムネイル、コンテンツ画像表示サイズ修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,10 +2,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_fit: [300, 200]
+  process resize_to_fit: [896, 504]
   # thumnail image
   version :thumb do
-   process resize_to_fit: [150, 100]
+   process resize_to_fit: [300, 200]
   end
 
   # Choose what kind of storage to use for this uploader:
