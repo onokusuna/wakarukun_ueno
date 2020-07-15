57bd76fa8f2bb04ece699139735bcff35fe87585
onokusuna <piroshki4315@gmail.com>
2020-07-15 14:26:47 +0900

    404,500エラーのrspecでのテストがテストを走らせる事態がうまくいかない

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -18,4 +18,20 @@ RSpec.feature "動作に関するテスト", type: :feature do
         expect(page).to have_content current_user.email
     end
   end
+  scenario '404エラーページに遷移し、ステータスコードが404であること' do
+    # 存在しないパスにアクセスする
+    visit '/users/0'
+
+    expect(page).to have_content '404 not found'
+    expect(page.status_code).to eq 404
+  end
+
+  scenario '500エラーページに遷移し、ステータスコードが500であること' do
+    # 一覧画面に遷移したら例外を発生させる
+    allow_any_instance_of(UsersController).to receive(:index).and_throw(Exception)
+    visit users_path
+
+    expect(page).to have_content '500 Internal Server Error'
+    expect(page.status_code).to eq 500
+  end
 end
